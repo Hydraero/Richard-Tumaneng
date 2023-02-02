934259e7e61b139bf86459335976e1bb6e87e7f0
Richard Tumaneng <ren.tumaneng@gmail.com>
2023-02-01 22:02:31 -0800

    Added Server and Client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    // generate manifest.json in outDir
-    manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-      input: '/path/to/main.tsx',
-    },
-  },
   plugins: [react()],
 })
